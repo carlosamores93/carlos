2ba3dad28b46270f25d4b0da1bc3b8b7c8b48a36
Carlos Salvador Amores Martinez <amorescarlos93@hotmail.com>
2018-03-22 23:58:01 +0100

    banner

---
+++ banner.php
@@ -0,0 +1,57 @@
+<!-- banner -->
+<div class="banner" id="home">
+	<div class="agileinfo-dot">
+		<div class="container">
+		<!-- header -->
+		<div class="header-w3layouts"> 
+			<!-- Navigation -->
+			<nav class="navbar navbar-default navbar-fixed-top"> 
+					<div class="navbar-header page-scroll">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Carlos Amores</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<h1><a class="navbar-brand" href="index.php"> Carlos Amores</a></h1>
+					</div> 
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav navbar-right cl-effect-15">
+							<!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+							<li class="hidden"><a class="page-scroll" href="#page-top"></a>	</li>
+							<li><a class="page-scroll scroll" href="#home">Home</a></li>
+							<li><a class="page-scroll scroll" href="#about">About</a></li>
+							<li><a class="page-scroll scroll" href="#skills">Skills</a></li>
+							<li><a class="page-scroll scroll" href="#education">Education/Experience </a></li>
+							<li><a class="page-scroll scroll" href="#contact">Contact</a></li>
+							<li><a class="page-scroll scroll" href="#map">Locate me</a></li>
+						</ul>
+					</div>
+					<!-- /.navbar-collapse -->
+				<!-- /.container -->
+			</nav>  
+		</div>	
+		<!-- //header -->
+
+			<div class="w3_banner_info">
+				<div class="w3_banner_info_grid">
+					<h2 data-aos="fade-right">Hola, soy </h2>
+					<h2 data-aos="fade-right">Carlos Amores</h2>
+					<h5>Desarrollador Web</h5>
+					<p> </p>
+					<ul data-aos="slide-up">
+						<li><a href="#" class="w3ls_more" data-toggle="modal" data-target="#myModal">Know More</a></li>
+						<!-- <li><a href="#" class="scroll w3l_contact"><i class="fa fa-download" aria-hidden="true"></i> Download CV</a></li> -->
+					</ul>
+				</div>
+			</div>
+			<div class="thim-click-to-bottom">
+				<a href="#about" class="scroll">
+					<i class="fa fa-arrows-v" aria-hidden="true"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- banner -->
