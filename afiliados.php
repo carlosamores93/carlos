36203c018f3c13e37dac978e4bebf36c35e28abc
Carlos Amores <carlos.amores@iahorro.com>
2018-03-23 11:35:54 +0100

    Afiliados ok

--- afiliados.php
+++ afiliados.php
@@ -1,15 +1,15 @@
 <div class="col-md-12" style="text-align:  center; background: white;height: 300px;">
-	<div class="col-md-4">
+	<div class="col-md-4 hidden-sm hidden-xs">
 		<a href="http://es.hostg.co/28017" target="_blank"><img style="border:0px;width:300px;" src="https://www.hostinger.com/banners/affiliate-banners/300x250a.jpg" width="300" height="250" alt=""></a>
 	
 	</div>
 
-	<div class="col-md-4">
+	<div class="col-md-4 hidden-sm hidden-xs">
 		<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS1=1&asins=B00H54W0VQ&linkId=907d27857f0793ce53153daaa650ee21&bc1=ffffff&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     </iframe>
 	</div>
 
-	<div class="col-md-4 hidden-xs">
+	<div class="col-md-4">
 		<iframe style="border:none;width:  300px;height:  250px;" src="https://rcm-eu.amazon-adsystem.com/e/cm?o=30&p=12&l=ur1&category=informatica&banner=1GZMTXW4K3DWBWD3VAR2&f=ifr&linkID=fddc5d0a9053b6f35460e4251c5faf46&t=amorescarlos9-21&tracking_id=amorescarlos9-21" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0" ></iframe>
 
 	</div>
