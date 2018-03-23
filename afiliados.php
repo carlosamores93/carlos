823878a834e8adccaed044414ee06f21808a1670
Carlos Amores <carlos.amores@iahorro.com>
2018-03-23 11:41:42 +0100

    Afiliados ok ok

--- afiliados.php
+++ afiliados.php
@@ -1,10 +1,10 @@
 <div class="col-md-12" style="text-align:  center; background: white;height: 300px;">
-	<div class="col-md-4 hidden-sm hidden-xs">
+	<div class="col-md-4 hidden-xs">
 		<a href="http://es.hostg.co/28017" target="_blank"><img style="border:0px;width:300px;" src="https://www.hostinger.com/banners/affiliate-banners/300x250a.jpg" width="300" height="250" alt=""></a>
 	
 	</div>
 
-	<div class="col-md-4 hidden-sm hidden-xs">
+	<div class="col-md-4 hidden-xs">
 		<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS1=1&asins=B00H54W0VQ&linkId=907d27857f0793ce53153daaa650ee21&bc1=ffffff&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     </iframe>
 	</div>
