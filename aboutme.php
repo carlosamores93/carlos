ae64519aff9bf0cef12ea52d6ebbc3bf87b0b117
Carlos Salvador Amores Martinez <amorescarlos93@hotmail.com>
2018-03-24 18:06:39 +0100

    estudiuios

--- aboutme.php
+++ aboutme.php
@@ -1,9 +1,16 @@
 <div class="about" id="about">
 	<h3 data-aos="zoom-in">Acerca de mi </h3>
 	<div class="col-md-6 about-left">
-		<p>Me llamo Carlos Salvador Amores Martínez, nací en 1 de Marzo de 1993 en <a href="http://www.lamana.gob.ec/">La Maná</a>, <a href="https://ecuadoramalavida.com.ec/">Ecuador</a></p>
+		<p>Me llamo Carlos Salvador Amores Martínez, nací en 1 de Marzo de 1993 en <a href="http://www.lamana.gob.ec/" target='_blank'>La Maná</a>, <a href="https://ecuadoramalavida.com.ec/" target='_blank'>Ecuador</a></p>
 		<p> Viví gran parte de mi infancia en mi pais de nacimiento. A los 7 años viaje a Madrid y es aquí donde he estudié parte de Primaria, ESO, el Bachiller y la Universidad.
 		</p>
+
+
+		<ul >
+			<li>Active item</li>
+			<li>Second item</li>
+			<li >Third item</li>
+		</ul>
 		<p> <strong> Primaria:</strong> de 3º  a 6º en el Colegio Dulce Nombre de Jesús, barrio Salamca, Madrid.</p>
 		<p> <strong> Educación Secundaria Obligatoria:</strong> de 1º a 4º ESO en el IES Carlos Mª Rodriguez de Valcárcel , barrio Moratalaz, Madrid.</p>
 		<p> <strong> Bachillerato:</strong> de 1º a 2º en el IES Felipe II , barrio Moratalaz, Madrid.</p>
