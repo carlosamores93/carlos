7da2a1db08cd71461b5d4db289563b32d1f0b451
Carlos Salvador Amores Martinez <amorescarlos93@hotmail.com>
2018-03-24 18:27:09 +0100

    educacion

---
+++ experiencia.php
@@ -0,0 +1,68 @@
+<div class="education" id="education">
+	    <div class="col-md-5 education-w3l">
+		     <h3 data-aos="zoom-in" class="w3l_head three">My Education</h3>
+			  <div class="education-agile-grids">
+				  <div class="education-agile-w3l">
+				     <div class="education-agile-w3l-year">
+					       <h4>2011-2016</h4>
+						   <h6>Grado Ingenieria de Computadores</h6>
+				     </div>
+					 <div class="education-agile-w3l-info">
+					       <h4>Universidad Complutense de Madrid</h4>
+						   <p>Facultad de Inf0rmática, nota media 7.1/10</p>
+						  
+				     </div>
+				      <div class="clearfix"></div>
+				  </div>
+				  <div class="education-agile-w3l two">
+				     <div class="education-agile-w3l-year">
+					       <h4>2009-2011</h4>
+						   <h6>Bachillerato</h6>
+				     </div>
+					 <div class="education-agile-w3l-info">
+					       <h4>I.E.S. Felipe II</h4>
+						   <p>Moratalaz, Madrid, España</p>
+						  
+				     </div>
+				      <div class="clearfix"></div>
+				  </div>
+				  <div class="education-agile-w3l">
+				     <div class="education-agile-w3l-year last">
+					       <h4>2005-2009</h4>
+						   <h6>Educacion Secundaria Obligatoria</h6>
+				     </div>
+					 <div class="education-agile-w3l-info last">
+					       <h4>I.E.S. Carlos Mª R. de Valcárcel</h4>
+						   <p>Moratalaz, Madrid, España</p>
+						  
+				     </div>
+				      <div class="clearfix"></div>
+				  </div>
+				 
+			  </div>
+		</div>
+		
+		<div data-aos="slide-up" class="col-md-2 middle">
+			<i class="fa fa-hourglass-end" aria-hidden="true"></i>
+		</div>
+		
+	    <div class="col-md-5 education-w3l">
+		     <h3 data-aos="zoom-in" class="w3l_head three">My Experience</h3>
+			  <div class="education-agile-grids">
+				  <div class="education-agile-w3l">
+				     <div class="education-agile-w3l-year">
+					       <h4>2013-2015</h4>
+						   <h6>Beca Desarrollo Web</h6>
+				     </div>
+					 <div class="education-agile-w3l-info">
+					       <h4>iAhorro B. S.</h4>
+						   <p>Beca en el deparamento de IT(backend) en iAhorro.com,</p>
+						  
+				     </div>
+				      <div class="clearfix"></div>
+				  </div>
+				 
+			  </div>
+		</div>
+		 <div class="clearfix"> </div>
+		</div>
--- index.php
+++ index.php
@@ -60,6 +60,12 @@
     
     ?>
 
+    <?php
+    
+    	require("experiencia.php");
+    
+    ?>
+
  	<?php
  	
     	require("contact.php");
