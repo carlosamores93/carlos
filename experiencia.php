be6187bfc36b720713798f8751e900bc6d704d56
Carlos Salvador Amores Martinez <amorescarlos93@hotmail.com>
2018-03-24 18:42:50 +0100

    Exp

--- experiencia.php
+++ experiencia.php
@@ -1,15 +1,42 @@
 <div class="experiencia" id="experiencia">
     <div class="text-center education-w3l">
-	     <h3 data-aos="zoom-in" class="w3l_head three">My Experience</h3>
+	     <h3 data-aos="zoom-in" class="w3l_head three">Experiencia</h3>
 		  <div class="education-agile-grids">
 			  <div class="education-agile-w3l">
 			     <div class="education-agile-w3l-year">
-				       <h4>2013-2015</h4>
-					   <h6>Beca Desarrollo Web</h6>
+				       <h4>2018</h4>
+					   <h6>Programador back-end</h6>
 			     </div>
 				 <div class="education-agile-w3l-info">
-				       <h4>iAhorro B. S.</h4>
-					   <p>Beca en el deparamento de IT(backend) en iAhorro.com,</p>
+				       <h4> <a href="https://www.iahorro.com" target="_blank">iAhorro</a></h4>
+					   <p> <strong>Parte de la plantila desde  marzo.</strong> Desarrollo web (Front End y Back End) y desarrollo de APIs RESTFul y webservices. Toma de requisitos, diseño y desarrollo de componentes de aplicación web y gestor de contenidos.Desarrollo de herramientas para adaptadas a las necesidades del departamento de marketing digital.
+					   </p>
+			     </div>
+			      <div class="clearfix"></div>
+			  </div>
+
+			  <div class="education-agile-w3l">
+			     <div class="education-agile-w3l-year">
+				       <h4>2017-2018</h4>
+					   <h6>Beca Departamneto IT</h6>
+			     </div>
+				 <div class="education-agile-w3l-info">
+				       <h4> <a href="https://www.iahorro.com" target="_blank">iAhorro</a></h4>
+					   <p>Beca de <strong> 9 meses</strong> 9 meses en el departamento de IT de iAhorro, perteneciente al grupo BC. Mantenimiento y evolución de componentes de código ya existentes. Desarrollo web (Front End y Back End) y desarrollo de APIs RESTFul y webservices.
+					   </p>
+					  
+			     </div>
+			      <div class="clearfix"></div>
+			  </div>
+
+			  <div class="education-agile-w3l">
+			     <div class="education-agile-w3l-year">
+				       <h4>2015</h4>
+					   <h6>Becario</h6>
+			     </div>
+				 <div class="education-agile-w3l-info">
+				       <h4>Cadmo/Conocimiento</h4>
+					   <p>Beca de <strong> 2 meses</strong> en el departamento de sistemas y control de calidad. Apoyo en la producción de contenidos replicando modelos existentes.o tareas relacionadas con el manejo de lenguajes de marcado y desarrollo web.</p>
 					  
 			     </div>
 			      <div class="clearfix"></div>
