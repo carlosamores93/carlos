a5acd428cfbbb143eb2fdaeef8dd472aaf159d26
Carlos Salvador Amores Martinez <amorescarlos93@hotmail.com>
2018-03-24 18:22:01 +0100

    Educaciuon

--- aboutme.php
+++ aboutme.php
@@ -4,14 +4,6 @@
 		<p>Me llamo Carlos Salvador Amores Martínez, nací en 1 de Marzo de 1993 en <a href="http://www.lamana.gob.ec/" target='_blank'>La Maná</a>, <a href="https://ecuadoramalavida.com.ec/" target='_blank'>Ecuador</a></p>
 		<p> Viví gran parte de mi infancia en mi pais de nacimiento. A los 7 años viaje a Madrid y es aquí donde he estudié parte de Primaria, ESO, el Bachiller y la Universidad.
 		</p>
-
-		<p>
-			<ul >
-				<li>Active item</li>
-				<li>Second item</li>
-				<li >Third item</li>
-			</ul>
-		</p>
 		<p> <strong> Primaria:</strong> de 3º  a 6º en el Colegio Dulce Nombre de Jesús, barrio Salamca, Madrid.</p>
 		<p> <strong> Educación Secundaria Obligatoria:</strong> de 1º a 4º ESO en el IES Carlos Mª Rodriguez de Valcárcel , barrio Moratalaz, Madrid.</p>
 		<p> <strong> Bachillerato:</strong> de 1º a 2º en el IES Felipe II , barrio Moratalaz, Madrid.</p>
--- skills.php
+++ skills.php
@@ -1,14 +1,14 @@
 <div class="skills" id="skills">
 	<div class="container">
-				<h3 data-aos="zoom-in">Skills</h3>
+				<h3 data-aos="zoom-in">Conocimientos</h3>
 		<div class="skill-grids">
 			<div class="col-md-6 skill-grids-left">
 				<div data-aos="flip-left" class="col-md-6 w3labout-img"> 
 				<div class="boxw3-agile"> 
 					<img src="images/s1.jpg" alt="" class="img-responsive" />
 					<div class="agile-caption">
-						<h3>Html5</h3>
-						<p>Lorem ipsum dolor sit amet.</p>
+						<h3>Web</h3>
+						<p>Conocimientos en leguajes de páginas web como HTML, PHP, CSS, jQuery, MySQL, AngularJS, framework Laravel, framework Bootstrap.</p>
 					</div> 
 				</div>
 				</div>
@@ -16,8 +16,8 @@
 				<div class="boxw3-agile"> 
 					<img src="images/s2.jpg" alt="" class="img-responsive" />
 					<div class="agile-caption">
-						<h3>Php</h3>
-						<p>Lorem ipsum dolor sit amet.</p>
+						<h3>Redes</h3>
+						<p>Conocimientos de los tipos de redes, protocolos, seguridad, electrónica y sistemas embebidos.</p>
 					</div> 
 				</div>
 				</div>
@@ -26,8 +26,8 @@
 				<div class="boxw3-agile"> 
 					<img src="images/s3.jpg" alt="" class="img-responsive" />
 					<div class="agile-caption">
-						<h3>Jquery</h3>
-						<p>Lorem ipsum dolor sit amet.</p>
+						<h3>C/C++</h3>
+						<p>Conocimientos C, C++ desarrollado con Visual Studio.</p>
 					</div> 
 				</div>
 				</div>
@@ -35,8 +35,8 @@
 				<div class="boxw3-agile"> 
 					<img src="images/s4.jpg" alt="" class="img-responsive" />
 					<div class="agile-caption">
-						<h3>JavaScript</h3>
-						<p>Lorem ipsum dolor sit amet.</p>
+						<h3>POO</h3>
+						<p>Conocimientos programación orientada objetos Java y C++, desarrollado con Eclipse </p>
 					</div> 
 				</div>
 				</div>
@@ -45,37 +45,37 @@
 			<div class="col-md-6 skill-grids-right">
 					
 			<!-- Skills -->
-			<div class="skillbar clearfix " data-percent="60%">
-				<div class="skillbar-title" style="background: #ff4f81;"><span>HTML5</span></div>
+			<div class="skillbar clearfix " data-percent="70%">
+				<div class="skillbar-title" style="background: #ff4f81;"><span>Java</span></div>
 				<div class="skillbar-bar" style="background: #ff4f81;"></div>
-				<div class="skill-bar-percent">60%</div>
+				<div class="skill-bar-percent">70%</div>
 			</div> <!-- End Skill Bar -->
 
-			<div class="skillbar clearfix " data-percent="50%">
-				<div class="skillbar-title" style="background: #ff9900;"><span>CSS3</span></div>
+			<div class="skillbar clearfix " data-percent="70%">
+				<div class="skillbar-title" style="background: #ff9900;"><span>Laravel</span></div>
 				<div class="skillbar-bar" style="background: #ff9900;"></div>
-				<div class="skill-bar-percent">50%</div>
+				<div class="skill-bar-percent">70%</div>
 			</div> <!-- End Skill Bar -->
 
 			<div class="skillbar clearfix " data-percent="60%">
-				<div class="skillbar-title" style="background: #8e43e7;"><span>jQuery</span></div>
+				<div class="skillbar-title" style="background: #8e43e7;"><span>C/C++</span></div>
 				<div class="skillbar-bar" style="background: #8e43e7;"></div>
 				<div class="skill-bar-percent">60%</div>
 			</div> <!-- End Skill Bar -->
 
-			<div class="skillbar clearfix " data-percent="80%">
-				<div class="skillbar-title" style="background: #146eb4;"><span>PHP</span></div>
+			<div class="skillbar clearfix " data-percent="50%">
+				<div class="skillbar-title" style="background: #146eb4;"><span>Angular</span></div>
 				<div class="skillbar-bar" style="background: #146eb4;"></div>
-				<div class="skill-bar-percent">80%</div>
+				<div class="skill-bar-percent">50%</div>
 			</div> <!-- End Skill Bar -->
 
-			<div class="skillbar clearfix " data-percent="80%">
-				<div class="skillbar-title" style="background: #11b563;"><span>Laravel</span></div>
+			<div class="skillbar clearfix " data-percent="70%">
+				<div class="skillbar-title" style="background: #11b563;"><span>MySQL</span></div>
 				<div class="skillbar-bar" style="background: #11b563;"></div>
-				<div class="skill-bar-percent">80%</div>
+				<div class="skill-bar-percent">70%</div>
 			</div> <!-- End Skill Bar -->
 
-				<p class="p1">...</p>
+				<p class="p1">Conocimientos Linux, comandos consola, sistemas de ficheros, gestión de fichero, gestión de procesos y memoria, comunicación entre procesos y núcleo SO (kernel).</p>
 			<!-- //Skills -->
 			</div>
 			<div class="clearfix"></div>
