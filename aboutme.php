61ae917e05618c0d696f1784b36b1c2f959ae153
Carlos Amores <carlos.amores@iahorro.com>
2018-04-23 16:00:49 +0200

    me

--- aboutme.php
+++ aboutme.php
@@ -1,9 +1,6 @@
 <div class="about" id="about">
 	<h3 data-aos="zoom-in">Acerca de mi </h3>
 	<div class="col-md-6 about-left" itemprop="description">
-		<p>Me llamo Carlos Salvador Amores Martínez, nací el 1 de Marzo de 1993 en <a href="http://www.lamana.gob.ec/"  itemprop="birthPlace" target='_blank'>La Maná</a>, <a itemprop="nationality" href="https://ecuadoramalavida.com.ec/" target='_blank'>Ecuador</a></p>
-		<p> Viví gran parte de mi infancia en mi pais de nacimiento. A los 7 años viaje a Madrid y es aquí donde estudié parte de Primaria, ESO, el Bachiller y la Universidad.
-		</p>
 		<p> <strong> Primaria:</strong> de 3º  a 6º en el Colegio Dulce Nombre de Jesús, barrio Salamanca, Madrid.</p>
 		<p> <strong> Educación Secundaria Obligatoria:</strong> de 1º a 4º ESO en el IES Carlos Mª Rodriguez de Valcárcel , barrio Moratalaz, Madrid.</p>
 		<p> <strong> Bachillerato:</strong> de 1º a 2º en el IES Felipe II , barrio Moratalaz, Madrid.</p>
