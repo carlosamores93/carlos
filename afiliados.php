8ed92425ae6ce2f7e7a4f3cadae4248cac00dd5b
Carlos Amores <carlos.amores@iahorro.com>
2018-11-27 15:35:04 +0100

    Afiliado

--- afiliados.php
+++ afiliados.php
@@ -1,17 +1,13 @@
 <div class="col-md-12" style="text-align:  center; background: white;height: 300px;">
-	<div class="col-md-4 hidden-xs">
-		<a href="http://es.hostg.co/28017" target="_blank"><img style="border:0px;width:300px;" src="https://www.hostinger.com/banners/affiliate-banners/300x250a.jpg" width="300" height="250" alt=""></a>
-	
-	</div>
 
-	<div class="col-md-4 hidden-xs">
-		<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS1=1&asins=B00H54W0VQ&linkId=907d27857f0793ce53153daaa650ee21&bc1=ffffff&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
+	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS1=1&asins=B01B2OR2VC&linkId=edab6c03a09bf2d6ed756c7d2fd3e027&bc1=FFFFFF&lt1=_top&fc1=333333&lc1=0066C0&bg1=FFFFFF&f=ifr">
     </iframe>
-	</div>
 
-	<div class="col-md-4">
-		<iframe style="border:none;width:  300px;height:  250px;" src="https://rcm-eu.amazon-adsystem.com/e/cm?o=30&p=12&l=ur1&category=informatica&banner=1GZMTXW4K3DWBWD3VAR2&f=ifr&linkID=fddc5d0a9053b6f35460e4251c5faf46&t=amorescarlos9-21&tracking_id=amorescarlos9-21" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0" ></iframe>
+    <a target="_blank" href="https://www.amazon.es/gp/product/B01B2OR2VC/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01B2OR2VC&linkCode=as2&tag=amorescarlos9-21&linkId=881c6581b79a61a69a72f1610e9ca382">KingDian 240 GB velocidad de lectura/escritura: 560/422mb/S SATA3 interna unidad de estado sólido para el ordenador portátil PC de escritorio con 256 M de caché</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01B2OR2VC" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
 
-	</div>
-	
+
+    <p>
+    	<a target="_blank" href="https://amzn.to/2P2c2X3">
+    KingDian 240 GB velocidad de lectura/escritura: 560/422mb/S SATA3 interna unidad de estado sólido para el ordenador portátil PC de escritorio con 256 M de caché</a>
+    </p>
 </div>
