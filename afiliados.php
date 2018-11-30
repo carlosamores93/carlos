d677451b350b6912fc6fe6d0377147d9ab0701e7
carlosamores <amorescarlos93@hotmail.com>
2018-11-30 17:51:05 +0100

    Afil 2

--- afiliados.php
+++ afiliados.php
@@ -4,12 +4,6 @@
     <div class="col-md-6 col-xs-6">
 		<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01N6JQS8C&linkId=a83a31fad275de0fe97c78f5d1b2ead7&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     	</iframe>
-    </div>
-
-
-
-    <div class="col-md-6 col-xs-6">
-    	
 	    <p>
 	    	<a target="_blank" href="https://www.amazon.es/gp/product/B01N6JQS8C/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01N6JQS8C&linkCode=as2&tag=amorescarlos9-21&linkId=b1cd102d237c40224fcc8cf3e9d2a9b4">Kingston SSD A400 - Disco duro sólido, 2.5", SATA 3, 120 GB</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01N6JQS8C" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
 	    </p>
@@ -20,6 +14,26 @@
 	    		Kingston SSD A400, 2.5", SATA 3, 120 GB
 	    	</a>
 	    </p>
+    </div>
+
+
+
+    <div class="col-md-6 col-xs-6">
+
+    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01ASVD2L4&linkId=627f1230fdbd3c6945cdbf62fc902199&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
+    	</iframe>
+
+    	<p>
+	    	<a target="_blank" href="https://www.amazon.es/gp/product/B01ASVD2L4/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01ASVD2L4&linkCode=as2&tag=amorescarlos9-21&linkId=22ba692e7644fd38b3ab593b7dc40d13">Fisher-Price - Codi-Oruga (Mattel DKT39)</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01ASVD2L4" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+	    </p>
+
+
+	    <p>
+	    	<a target="_blank" href="https://amzn.to/2KM5VFV">
+	    		Oruga
+	    	</a>
+	    </p>
+    	
     </div>
 
 
