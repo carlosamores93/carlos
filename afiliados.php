5e8ad7cfe3de3ea7868aa0758643faffad5ea35a
carlosamores <amorescarlos93@hotmail.com>
2018-11-30 23:07:08 +0100

    SSd adaptador fix

--- afiliados.php
+++ afiliados.php
@@ -1,42 +1,38 @@
 <div class="col-md-12" style="text-align:  center; background: white;height: 300px;">
 
 
-    <div class="col-md-4 col-xs-4">
+    <div class="col-md-3 col-xs-3">
 		<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01N6JQS8C&linkId=a83a31fad275de0fe97c78f5d1b2ead7&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     	</iframe>
 	    <p>
 	    	<a target="_blank" href="https://www.amazon.es/gp/product/B01N6JQS8C/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01N6JQS8C&linkCode=as2&tag=amorescarlos9-21&linkId=b1cd102d237c40224fcc8cf3e9d2a9b4">Kingston SSD A400 - Disco duro sólido, 2.5", SATA 3, 120 GB</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01N6JQS8C" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
-	    </p>
-
-
-	    <p>
 	    	<a target="_blank" href="https://amzn.to/2DT0x2i">
 	    		Kingston SSD A400, 2.5", SATA 3, 120 GB
 	    	</a>
 	    </p>
+
+
     </div>
 
 
 
-    <div class="col-md-4 col-xs-4">
+    <div class="col-md-3 col-xs-3">
 
     	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01ASVD2L4&linkId=627f1230fdbd3c6945cdbf62fc902199&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     	</iframe>
 
     	<p>
 	    	<a target="_blank" href="https://www.amazon.es/gp/product/B01ASVD2L4/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01ASVD2L4&linkCode=as2&tag=amorescarlos9-21&linkId=22ba692e7644fd38b3ab593b7dc40d13">Fisher-Price - Codi-Oruga (Mattel DKT39)</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01ASVD2L4" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
-	    </p>
-
-
-	    <p>
 	    	<a target="_blank" href="https://amzn.to/2KM5VFV">
 	    		Oruga
 	    	</a>
 	    </p>
+
+
     	
     </div>
 
-    <div class="col-md-4 col-xs-4">
+    <div class="col-md-3 col-xs-3">
 
     	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B00G4UZEKG&linkId=499e9b725267f21161b8b2a415cd5f0a&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     	</iframe>
@@ -50,7 +46,7 @@
     </div>
 
 
-    <div class="col-md-4 col-xs-4">
+    <div class="col-md-3 col-xs-3">
 
     	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B00C94G7Q6&linkId=91a5c487f4a2c8e1919b009008162327&bc1=000000&lt1=_blank&fc1=ff0000&lc1=0c5fad&bg1=ffffff&f=ifr">
     	</iframe>
