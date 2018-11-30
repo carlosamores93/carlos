5c6123c6fa665ed3bafd2d7a9ce3ee831a76e119
carlosamores <amorescarlos93@hotmail.com>
2018-11-30 23:04:38 +0100

    SSd adaptador

--- afiliados.php
+++ afiliados.php
@@ -1,7 +1,7 @@
 <div class="col-md-12" style="text-align:  center; background: white;height: 300px;">
 
 
-    <div class="col-md-6 col-xs-6">
+    <div class="col-md-4 col-xs-4">
 		<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01N6JQS8C&linkId=a83a31fad275de0fe97c78f5d1b2ead7&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     	</iframe>
 	    <p>
@@ -18,7 +18,7 @@
 
 
 
-    <div class="col-md-6 col-xs-6">
+    <div class="col-md-4 col-xs-4">
 
     	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01ASVD2L4&linkId=627f1230fdbd3c6945cdbf62fc902199&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     	</iframe>
@@ -31,6 +31,33 @@
 	    <p>
 	    	<a target="_blank" href="https://amzn.to/2KM5VFV">
 	    		Oruga
+	    	</a>
+	    </p>
+    	
+    </div>
+
+    <div class="col-md-4 col-xs-4">
+
+    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B00G4UZEKG&linkId=499e9b725267f21161b8b2a415cd5f0a&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
+    	</iframe>
+
+	    <p>
+	    	<a target="_blank" href="https://amzn.to/2KIAz2V">
+	    		Salcar - 2.HDD SATA-SATA 3.0 (SATA I II III) Bahía de disco duro 2nd ODD SSD Hard Drive Caddy 9,5mm universal portátil de CD/DVD-ROM para Notebooks sustituye SuperDrive
+	    	</a>
+	    </p>
+    	
+    </div>
+
+
+    <div class="col-md-4 col-xs-4">
+
+    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B00C94G7Q6&linkId=91a5c487f4a2c8e1919b009008162327&bc1=000000&lt1=_blank&fc1=ff0000&lc1=0c5fad&bg1=ffffff&f=ifr">
+    	</iframe>
+
+	    <p>
+	    	<a target="_blank" href="https://www.amazon.es/gp/product/B00C94G7Q6/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B00C94G7Q6&linkCode=as2&tag=amorescarlos9-21&linkId=922edccf73dd4bf0c836e78049637290">
+	    		QUMOX Adaptador de 2do HDD SSD SATA para Disco Duro de Laptops Optical Bay 9.5mm
 	    	</a>
 	    </p>
     	
