882d9eee3494f071e0b3e72733974c5591c65235
carlosamores <amorescarlos93@hotmail.com>
2018-12-03 23:11:48 +0100

    Afi

--- afiliados.php
+++ afiliados.php
@@ -1,22 +1,9 @@
 <div class="col-md-12" style="text-align:  center; background: white;height: 300px;">
 
 
-    <div class="col-md-3 col-xs-3">
-		<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01N6JQS8C&linkId=a83a31fad275de0fe97c78f5d1b2ead7&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
-    	</iframe>
-	    <p>
-	    	<a target="_blank" href="https://www.amazon.es/gp/product/B01N6JQS8C/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01N6JQS8C&linkCode=as2&tag=amorescarlos9-21&linkId=b1cd102d237c40224fcc8cf3e9d2a9b4">Kingston SSD A400 - Disco duro sólido, 2.5", SATA 3, 120 GB</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01N6JQS8C" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
-	    	<a target="_blank" href="https://amzn.to/2DT0x2i">
-	    		Kingston SSD A400, 2.5", SATA 3, 120 GB
-	    	</a>
-	    </p>
 
 
-    </div>
-
-
-
-    <div class="col-md-3 col-xs-3">
+    <div class="col-md-6 col-xs-6">
 
     	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01ASVD2L4&linkId=627f1230fdbd3c6945cdbf62fc902199&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     	</iframe>
@@ -32,32 +19,17 @@
     	
     </div>
 
-    <div class="col-md-3 col-xs-3">
-
-    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B00G4UZEKG&linkId=499e9b725267f21161b8b2a415cd5f0a&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
+    <div class="col-md-6 col-xs-6">
+    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B014SVVLDQ&linkId=05f80e565034d31abacd28b9e1591a53&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
     	</iframe>
 
-	    <p>
-	    	<a target="_blank" href="https://amzn.to/2KIAz2V">
-	    		Salcar - 2.HDD SATA-SATA 3.0 (SATA I II III) Bahía de disco duro 2nd ODD SSD Hard Drive Caddy 9,5mm universal portátil de CD/DVD-ROM para Notebooks sustituye SuperDrive
-	    	</a>
-	    </p>
-    	
+    	<p>
+    		<a target="_blank" href="https://www.amazon.es/gp/product/B014SVVLDQ/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B014SVVLDQ&linkCode=as2&tag=amorescarlos9-21&linkId=cdf55a3416b73dbe8a9759cd5365f1e3">Ravensburger 7033 Paw Patrol - Paquete de 4 puzzles en la caja, con Paw Patrol</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B014SVVLDQ" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+    	</p>
+
     </div>
 
 
-    <div class="col-md-3 col-xs-3">
-
-    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=tf_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B00C94G7Q6&linkId=91a5c487f4a2c8e1919b009008162327&bc1=000000&lt1=_blank&fc1=ff0000&lc1=0c5fad&bg1=ffffff&f=ifr">
-    	</iframe>
-
-	    <p>
-	    	<a target="_blank" href="https://www.amazon.es/gp/product/B00C94G7Q6/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B00C94G7Q6&linkCode=as2&tag=amorescarlos9-21&linkId=922edccf73dd4bf0c836e78049637290">
-	    		QUMOX Adaptador de 2do HDD SSD SATA para Disco Duro de Laptops Optical Bay 9.5mm
-	    	</a>
-	    </p>
-    	
-    </div>
 
 
 
