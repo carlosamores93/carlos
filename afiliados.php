eeb5b8a93cc8139c6c6edf60469181a07af7e2c2
carlosamores <amorescarlos93@hotmail.com>
2018-12-10 00:45:10 +0100

    Altavoz

--- afiliados.php
+++ afiliados.php
@@ -3,30 +3,21 @@
 
 
 
-    <div class="col-md-6 col-xs-6">
+    <div class="col-md-12 col-xs-12">
 
-    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01ASVD2L4&linkId=627f1230fdbd3c6945cdbf62fc902199&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
-    	</iframe>
+    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01LKZS2H6&linkId=19a0d75f763b13f82e16d577ad30c97a&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
+        </iframe>
 
     	<p>
-	    	<a target="_blank" href="https://www.amazon.es/gp/product/B01ASVD2L4/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01ASVD2L4&linkCode=as2&tag=amorescarlos9-21&linkId=22ba692e7644fd38b3ab593b7dc40d13">Fisher-Price - Codi-Oruga (Mattel DKT39)</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01ASVD2L4" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
-	    	<a target="_blank" href="https://amzn.to/2KM5VFV">
-	    		Oruga
+	    	<a target="_blank" href="https://www.amazon.es/gp/product/B01LKZS2H6/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01LKZS2H6&linkCode=as2&tag=amorescarlos9-21&linkId=d9c101fa05e42d1ea6c72becf3a5c41b">ICONNTECHS IT Altavoz Bluetooth Portátil para Exterior Ducha Agua Casa Altavoces Estéreo IPX6 Resistente al Agua 7 Horas</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01LKZS2H6" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+            
+	    	<a target="_blank" href="https://amzn.to/2Pt5uAU">
+	    		Altavoz
 	    	</a>
 	    </p>
 
 
     	
-    </div>
-
-    <div class="col-md-6 col-xs-6">
-    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B014SVVLDQ&linkId=05f80e565034d31abacd28b9e1591a53&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
-    	</iframe>
-
-    	<p>
-    		<a target="_blank" href="https://www.amazon.es/gp/product/B014SVVLDQ/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B014SVVLDQ&linkCode=as2&tag=amorescarlos9-21&linkId=cdf55a3416b73dbe8a9759cd5365f1e3">Ravensburger 7033 Paw Patrol - Paquete de 4 puzzles en la caja, con Paw Patrol</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B014SVVLDQ" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
-    	</p>
-
     </div>
 
 
