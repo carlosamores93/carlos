b422069cc83cf8847e9deda85ab8399d85444f75
carlosamores <amorescarlos93@hotmail.com>
2018-12-18 20:58:18 +0100

    mas

--- afiliados.php
+++ afiliados.php
@@ -3,21 +3,38 @@
 
 
 
-    <div class="col-md-12 col-xs-12">
+    <div class="col-md-6 col-xs-6">
 
-    	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01LKZS2H6&linkId=19a0d75f763b13f82e16d577ad30c97a&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
-        </iframe>
+        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01M4KMFXR&linkId=c0306df45d7170cf0759ca30630d94e5&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
+    </iframe>
 
-    	<p>
-	    	<a target="_blank" href="https://www.amazon.es/gp/product/B01LKZS2H6/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01LKZS2H6&linkCode=as2&tag=amorescarlos9-21&linkId=d9c101fa05e42d1ea6c72becf3a5c41b">ICONNTECHS IT Altavoz Bluetooth Portátil para Exterior Ducha Agua Casa Altavoces Estéreo IPX6 Resistente al Agua 7 Horas</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01LKZS2H6" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+        <p>
+            <a target="_blank" href="https://www.amazon.es/gp/product/B01M4KMFXR/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01M4KMFXR&linkCode=as2&tag=amorescarlos9-21&linkId=72b33415a3c72a0d9c6f9412a5bcd604">ABYED Pijama Animal Entero Unisex para Adultos Niños con Capucha Ropa de Dormir Traje de Disfraz para Festival de Carnaval Halloween Navidad</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01M4KMFXR" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
             
-	    	<a target="_blank" href="https://amzn.to/2Pt5uAU">
-	    		Altavoz
-	    	</a>
-	    </p>
+            <a target="_blank" href="https://amzn.to/2BrtP5i">
+                ABYED Pijama Animal Entero Unisex NIÑOSSSSSSSS
+            </a>
+        </p>
 
 
-    	
+        
+    </div>
+
+    <div class="col-md-6 col-xs-6">
+
+        <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=amorescarlos9-21&m=amazon&o=30&p=8&l=as1&IS2=1&asins=B01M9CHY5F&linkId=9dbe73682d9c9aa82913c32d3dc8faf7&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
+    </iframe>
+
+        <p>
+            <a target="_blank" href="https://www.amazon.es/gp/product/B01M9CHY5F/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01M9CHY5F&linkCode=as2&tag=amorescarlos9-21&linkId=32dd0e98d33bd4eafcbb03b069828b82">ABYED Pijama Animal Entero Unisex para Adultos Niños con Capucha Ropa de Dormir Traje de Disfraz para Festival de Carnaval Halloween Navidad</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=amorescarlos9-21&l=am2&o=30&a=B01M9CHY5F" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+            
+            <a target="_blank" href="https://amzn.to/2QG5KSS">
+                ABYED Pijama Animal Entero Unisex ADULTO
+            </a>
+        </p>
+
+
+        
     </div>
 
 
